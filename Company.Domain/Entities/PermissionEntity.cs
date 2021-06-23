70beae00d750c1a1f41ec867f06d4b464c702a15
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2021-06-23 04:40:10 -0500

    :construction: Add Type property in permission entity

--- Company.Domain/Entities/PermissionEntity.cs
+++ Company.Domain/Entities/PermissionEntity.cs
@@ -5,8 +5,9 @@ namespace Company.Domain
     public class PermissionEntity
     {
         public Guid Id { get; set; }
-        public int Order { get; set; }
+        public string Type { get; set; }
         public string Name { get; set; }
         public string DisplayName { get; set; }
+        public int Order { get; set; }
     }
 }
