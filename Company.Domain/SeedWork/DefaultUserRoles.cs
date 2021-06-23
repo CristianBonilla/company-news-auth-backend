12b58a33b42581a72215e52684e992c691aae0b9
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2021-06-23 12:05:04 -0500

    :recycle: Refactor the user and role defaults, defining role entity instances

--- Company.Domain/SeedWork/DefaultUserRoles.cs
+++ Company.Domain/SeedWork/DefaultUserRoles.cs
@@ -4,25 +4,34 @@ namespace Company.Domain
 {
     public class DefaultUserRoles
     {
-        public static (Guid, string, PermissionEntity[]) AdminUser { get; } = (
-            new Guid("c651f92d-d9a1-45e8-9d04-3bb184da7a96"),
-            nameof(AdminUser),
+        public static (RoleEntity Role, PermissionEntity[] Permissions) AdminUser { get; } = (
+            new()
+            {
+               Id = new Guid("c651f92d-d9a1-45e8-9d04-3bb184da7a96"),
+               Name = nameof(AdminUser),
+               DisplayName = "Administrador"
+            },
             new PermissionEntity[]
             {
                 Roles.ALL,
                 Users.ALL,
                 News.ALL
             });
-        public static (Guid, string, PermissionEntity[]) VisitorUser { get; } = (
-            new Guid("590cb67a-5177-4bda-86e4-e5b80ac333c9"),
-            nameof(VisitorUser),
-            new PermissionEntity[]
+        public static (RoleEntity Role, PermissionEntity[] Permissions) VisitorUser { get; } = (
+            new()
             {
-                News.ALL
-            });
-        public static (Guid, string, PermissionEntity[]) AssistantUser { get; } = (
-            new Guid("f7b2beba-7445-463d-8978-0b3b3940390c"),
-            nameof(AssistantUser),
+                Id = new Guid("590cb67a-5177-4bda-86e4-e5b80ac333c9"),
+                Name = nameof(VisitorUser),
+                DisplayName = "Visitante"
+            },
+            new[] { News.ALL });
+        public static (RoleEntity Role, PermissionEntity[] Permissions) AssistantUser { get; } = (
+            new()
+            {
+                Id = new Guid("f7b2beba-7445-463d-8978-0b3b3940390c"),
+                Name = nameof(AssistantUser),
+                DisplayName = "Asistente"
+            },
             new PermissionEntity[]
             {
                 Roles.GetRoles,
@@ -31,9 +40,13 @@ namespace Company.Domain
                 Users.GetUserById,
                 News.ALL
             });
-        public static (Guid, string, PermissionEntity[]) EditorUser { get; } = (
-            new Guid("3cb57f96-7092-4842-a3a4-5e8302f6618d"),
-            nameof(EditorUser),
+        public static (RoleEntity Role, PermissionEntity[] Permissions) EditorUser { get; } = (
+            new()
+            {
+                Id = new Guid("3cb57f96-7092-4842-a3a4-5e8302f6618d"),
+                Name = nameof(EditorUser),
+                DisplayName = "Editor"
+            },
             new PermissionEntity[]
             {
                 Roles.GetRoles,
