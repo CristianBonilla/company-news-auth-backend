0f28b6be2050e02c7925d3f4a1a72345abd841f2
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2021-06-23 22:38:32 -0500

    :construction: Add repository interfaces to include permission entity repositories

--- Company.Domain/Context/IRepositories.cs
+++ Company.Domain/Context/IRepositories.cs
@@ -4,6 +4,10 @@ namespace Company.Domain
 {
     public interface IRoleRepository : IRepository<CompanyContext, RoleEntity> { }
 
+    public interface IPermissionRepository : IRepository<CompanyContext, PermissionEntity> { }
+
+    public interface IRolePermissionRepository : IRepository<CompanyContext, RolePermissionEntity> { }
+
     public interface IUserRepository : IRepository<CompanyContext, UserEntity> { }
 
     public interface INewsRepository : IRepository<CompanyContext, NewsEntity> { }
