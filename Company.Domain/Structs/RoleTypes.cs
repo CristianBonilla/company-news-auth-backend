96248a601cdcd8b8fe2e7074ecc488cc735edf24
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2021-06-27 21:23:28 -0500

    :recycle: A structure to get the existing role types

---
+++ Company.Domain/Structs/RoleTypes.cs
@@ -0,0 +1,10 @@
+namespace Company.Domain
+{
+    public struct RoleTypes
+    {
+        public const string VisitorUser = nameof(VisitorUser);
+        public const string AssistantUser = nameof(AssistantUser);
+        public const string EditorUser = nameof(EditorUser);
+        public const string AdminUser = nameof(AdminUser);
+    }
+}
